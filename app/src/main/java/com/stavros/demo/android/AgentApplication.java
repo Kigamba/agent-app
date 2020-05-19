f5981ac5a91d50c742fb0fd0c93d61ffcc0d8c0e
Ephraim Kigamba <n.kigamba@students.ku.ac.ke>
2020-05-19 21:55:17 +0300

    Move activities to form package

---
+++ app/src/main/java/com/stavros/demo/android/AgentApplication.java
@@ -0,0 +1,9 @@
+package com.stavros.demo.android;
+
+import android.app.Application;
+
+/**
+ * Created by Kigamba (nek.eam@gmail.com) on 19-May-2020
+ */
+public class AgentApplication extends Application {
+}
